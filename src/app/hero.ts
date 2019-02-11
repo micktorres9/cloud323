862ac18c069134d06f6494a34d4ee4208965d7d2
mick torres <micklunatorres9@gmail.com>
2019-02-10 17:04:08 -0700

    this is the initial commit

---
+++ src/app/hero.ts
@@ -0,0 +1,10 @@
+/*
+@author:        Mick Torres
+@classname:     Hero
+@description:   Hero has two variables of types number and string, for id and name respectively
+ */
+
+export class Hero {
+    id: number;
+    name: string;
+}
